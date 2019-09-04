afa7ad3b91dbab351314465388630f81d3889768
jmolla31 <jmolla31@gmail.com>
2019-09-04 23:21:27 +0200

    Added new sample class FullWar.cs and Startup file for dependency injection

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mdaWar
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
